c9e6e2a55d8a8d4a2a07b1bfae2cb17ed41d2b56
Friedrich Loparev <friedrich.loparev@allvest.de>
2023-05-26 15:19:17 +0200

    Create starting field

---
+++ exercise_4/main.py
@@ -0,0 +1,4 @@
+from exercise_4.model import create_empty_field
+
+A = create_empty_field(5, 5)
+print(A)
---
+++ exercise_4/model.py
@@ -0,0 +1,8 @@
+import numpy as np
+def create_empty_field(rows: int, columns: int) -> np.ndarray:
+    return np.zeros( (rows, columns), dtype=np.int32)
+
+def calculate_successor_states():
+    pass
+
+
