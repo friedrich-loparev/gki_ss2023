d0fc6c587b6a011f52edb1e8364377efb6944f83
friedrich <friedrich.loparev@protonmail.ch>
2023-05-29 14:44:22 +0000

    Implement Board and Move. Test Board and make_move function.

--- exercise_4/main.py
+++ exercise_4/main.py
@@ -1,4 +1,14 @@
-from exercise_4.model import create_empty_field
+from exercise_4.model import Board, Move
 
-A = create_empty_field(5, 5)
-print(A)
+board = Board()
+board.create_start_board(3, 3)
+print(board.current_state)
+
+left_bottom_V = Move([(2, 0), (1, 0)], 'V')
+right_bottom_H = Move([(2, 1), (2, 2)], 'H')
+
+board.make_move(left_bottom_V)
+print(board.current_state)
+
+board.make_move(right_bottom_H)
+print(board.current_state)
--- exercise_4/model.py
+++ exercise_4/model.py
@@ -1,8 +1,47 @@
-import numpy as np
-def create_empty_field(rows: int, columns: int) -> np.ndarray:
-    return np.zeros( (rows, columns), dtype=np.int32)
+import sys
 
-def calculate_successor_states():
+import numpy as np
+
+
+class Move:
+    coordinates: list[tuple]
+    player: str
+
+    def __init__(self, coordinates, player):
+        self.coordinates = coordinates
+        self.player = player
+
+
+class Board:
+    """Represents state of the board"""
+
+    current_state: np.ndarray
+
+    def make_move(self, move: Move):
+        """Takes next move as input and performs the move."""
+        try:
+            if move.player == 'V':
+                for position in move.coordinates:
+                    self.current_state[position] = 2
+            elif move.player == 'H':
+                for position in move.coordinates:
+                    self.current_state[position] = 1
+        except KeyError:
+            sys.exit(1)
+
+    def create_start_board(self, rows: int, columns: int) -> None:
+        self.current_state = np.zeros((rows, columns), dtype=np.int32)
+
+
+def game_over(board: Board) -> bool:
     pass
 
 
+def evaluate(board: Board):
+    """A function that evaluates the current state of the board and returns a score."""
+    pass
+
+
+def get_possible_moves(board: Board):
+    """A function that returns a list of all possible moves from the current state."""
+    pass
